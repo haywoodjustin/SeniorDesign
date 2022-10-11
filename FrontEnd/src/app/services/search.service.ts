70206a0640d8df8370c0c7e4fec48f5c801660bd
haywoodjustin <49731705+haywoodjustin@users.noreply.github.com>
2022-10-11 13:01:41 -0400

    Random

--- FrontEnd/src/app/services/search.service.ts
+++ FrontEnd/src/app/services/search.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, Observable, Subject } from "rxjs";
+import { map, Observable } from "rxjs";
 import { SongRequest, JSONSongRequest } from "../queue/song-request";
 
 

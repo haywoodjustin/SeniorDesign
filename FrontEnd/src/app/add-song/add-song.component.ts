09a32c38b04e4f761ab2f58cba197e12005a1db2
haywoodjustin <49731705+haywoodjustin@users.noreply.github.com>
2022-10-17 16:28:02 -0400

    Search Service cleaned up to reflect Queue Service

--- FrontEnd/src/app/add-song/add-song.component.ts
+++ FrontEnd/src/app/add-song/add-song.component.ts
@@ -15,36 +15,24 @@ import { MessageService } from 'primeng/api';
 export class AddSongComponent implements OnInit{
   protected search = new FormControl();  
 
-  private searchText: string = '';  
+  songs$ = new Observable<SongRequest[]>(); 
 
-  public songs: SongRequest[] = []; 
-
-  public selectedSong: SongRequest | undefined; 
+  protected selectedSong: SongRequest | undefined; 
 
   constructor(private qs: QueueService, private ss: SearchService, private message: MessageService) { }
 
   ngOnInit(): void {
+    // sets local observable equal to service observable 
+    // whens service uses "next" this observable picks it up and displays it 
+    this.songs$ = this.ss.songs$;
+    
     //subscribe to value change of search form and fire get songs after debounce time 
     this.search.valueChanges.pipe(
       debounceTime(400),
       distinctUntilChanged()
       ).subscribe(term => {
-        this.searchText = term;
-        this.getSongs(term); 
-      }); 
-  }
-
-  getSongs(searchText: string) {
-    if(!searchText) {
-      this.songs = [];
-      return; 
-    }
-    // ss.getSongs returns observable 
-    // subscribe reads observable data and assigns it to local array to display
-    //TODO: Implement error catching and handling 
-    this.ss.getSongs(searchText).subscribe(songs => {
-      this.songs = songs; 
-    })
+        this.ss.getSongs(term); 
+      });  
   }
  
   //TODO Reset form control and unselect clicked row on table 
